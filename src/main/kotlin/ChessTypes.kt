b061605770486c31a0555660fc33e77165a74357
larry.charry <larry.charry@kakaopaycorp.com>
2021-02-27 11:44:40 +0900

    move 로직 보완 진행중 (아직)

--- src/main/kotlin/ChessTypes.kt
+++ src/main/kotlin/ChessTypes.kt
@@ -14,4 +14,30 @@ sealed class Tile(open val coord: Coord) {
     class Blank(coord: Coord) : Tile(coord)
 }
 
+enum class Direction {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST,
+    NNE,
+    NNW,
+    SSE,
+    SSW,
+    EEN,
+    EES,
+    WWN,
+    WWS;
 
+    companion object {
+        fun linearDirection(): Array<Direction> = arrayOf(NORTH, EAST, SOUTH, WEST)
+        fun diagonalDirection(): Array<Direction> = arrayOf(NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST)
+        fun everyDirection(): Array<Direction> = arrayOf(NORTH, EAST, SOUTH, WEST, NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST)
+        fun knightDirection(): Array<Direction> = arrayOf(NNE, NNW, SSE, SSW, EEN, EES, WWN, WWS)
+        fun whitePawnDirection(): Array<Direction> = arrayOf(NORTH, NORTHEAST, NORTHWEST)
+        fun blackPawnDirection(): Array<Direction> = arrayOf(SOUTH, SOUTHEAST, SOUTHWEST)
+    }
+}
--- src/main/kotlin/PieceDynamicFunctions.kt
+++ src/main/kotlin/PieceDynamicFunctions.kt
@@ -12,22 +12,27 @@ fun positionPiece(coord: Coord, color: Color, piece: Piece): (Board) -> Board =
     }
 
 val findPiece: (String) -> (Board) -> Tile? =
-    { s ->
-        { b ->
-            b.tiles.find { it.coord == coordOfString(s) }
+    { coord ->
+        { board ->
+            board.tiles.find { it.coord == coordOfString(coord) }
         }
     }
 
 fun move(board: Board, source: String, dest: String): Board {
+    val sourceCoord = coordOfString(source)
+    val destCoord = coordOfString(dest)
     val sourceTile: Tile =
         board.tiles.find { it.coord == coordOfString(source) } ?: throw RuntimeException("wrong position")
+
     tailrec fun helper(rest: List<Tile>, acc: List<Tile>): List<Tile> {
         return when {
             rest.isEmpty() -> acc
-            movable() && rest.head().coord == coordOfString(source) -> {
+            movable(sourceTile)(destCoord)(rest.head())
+                    && rest.head().coord == sourceCoord -> {
                 helper(rest.tail(), acc + Tile.Blank(rest.head().coord))
             }
-            movable() && rest.head().coord == coordOfString(dest)
+            movable(sourceTile)(destCoord)(rest.head())
+                    && rest.head().coord == destCoord
                     && sourceTile is Tile.ColoredPiece -> {
                 helper(rest.tail(), acc + Tile.ColoredPiece(sourceTile.color, sourceTile.piece, rest.head().coord))
             }
@@ -37,7 +42,67 @@ fun move(board: Board, source: String, dest: String): Board {
     return Board(helper(board.tiles, emptyList()))
 }
 
-val movable: () -> Boolean = {
-    // 각 기물간 이동가능여부 파악
-    true
-}
+val movable: (source: Tile) -> (dest: Coord) -> (now: Tile) -> Boolean =
+    { source ->
+        { dest ->
+            { now ->
+                when (source) {
+                    is Tile.Blank -> true
+                    is Tile.ColoredPiece -> {
+                        val vector = calculateVector(source.coord)(dest)
+                        if (vector == null) false
+                        else possibleDirections(source.piece)(source.color).find { it == vector } != null
+                    }
+                }
+            }
+        }
+    }
+
+val applyDirection: (Direction) -> (Coord) -> Coord =
+    { direction ->
+        { coord ->
+            when (direction) {
+                Direction.NORTH -> Coord(coord.rank + 1, coord.file)
+                Direction.NORTHEAST -> Coord(coord.rank + 1, coord.file + 1)
+                Direction.EAST -> Coord(coord.rank, coord.file + 1)
+                Direction.SOUTHEAST -> Coord(coord.rank - 1, coord.file + 1)
+                Direction.SOUTH -> Coord(coord.rank - 1, coord.file)
+                Direction.SOUTHWEST -> Coord(coord.rank - 1, coord.file - 1)
+                Direction.WEST -> Coord(coord.rank, coord.file - 1)
+                Direction.NORTHWEST -> Coord(coord.rank + 1, coord.file - 1)
+                Direction.NNE -> Coord(coord.rank + 2, coord.file + 1)
+                Direction.NNW -> Coord(coord.rank + 2, coord.file - 1)
+                Direction.SSE -> Coord(coord.rank - 2, coord.file + 1)
+                Direction.SSW -> Coord(coord.rank - 2, coord.file - 1)
+                Direction.EEN -> Coord(coord.rank + 1, coord.file + 2)
+                Direction.EES -> Coord(coord.rank - 1, coord.file + 2)
+                Direction.WWN -> Coord(coord.rank + 1, coord.file - 2)
+                Direction.WWS -> Coord(coord.rank - 1, coord.file - 2)
+            }
+        }
+    }
+
+val possibleDirections: (Piece) -> (Color) -> Array<Direction> =
+    { piece ->
+        { color ->
+            when (piece) {
+                Piece.PAWN -> {
+                    if (color == Color.WHITE) Direction.whitePawnDirection()
+                    else Direction.blackPawnDirection()
+                }
+                Piece.KING -> Direction.everyDirection()
+                Piece.QUEEN -> Direction.everyDirection()
+                Piece.ROOK -> Direction.linearDirection()
+                Piece.BISHOP -> Direction.diagonalDirection()
+                Piece.KNIGHT -> Direction.knightDirection()
+            }
+        }
+    }
+
+// 재귀로 여러번 돌려야 할듯. 벡터도 아님.. 벡터는 방향과 크기를 가져야함
+val calculateVector: (source: Coord) -> (dest: Coord) -> Direction? =
+    { source ->
+        { dest ->
+            Direction.values().find { applyDirection(it)(source) == dest }
+        }
+    }
